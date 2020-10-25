be0bc452436669c712876386bc887c76cc8eee47
Juan J LF <justin@.local>
2020-10-25 19:21:15 -0400

    v 1.6.6

--- Example/Pods/Target Support Files/JJGuiso/JJGuiso-umbrella.h
+++ Example/Pods/Target Support Files/JJGuiso/JJGuiso-umbrella.h
@@ -10,15 +10,6 @@
 #endif
 #endif
 
-#import "config.h"
-#import "decode.h"
-#import "demux.h"
-#import "encode.h"
-#import "format_constants.h"
-#import "mux.h"
-#import "mux_types.h"
-#import "types.h"
-#import "WebP.h"
 
 FOUNDATION_EXPORT double JJGuisoVersionNumber;
 FOUNDATION_EXPORT const unsigned char JJGuisoVersionString[];
